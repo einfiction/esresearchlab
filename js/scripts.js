6ee327f0b247f8fc874ea99f923ecb101874e7f6
pcversal <info@pcversalstudios.com>
2024-05-29 00:45:15 +0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Sample JavaScript for basic interactivity
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Example: Smooth scrolling for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
